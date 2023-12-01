111b6c5d395f2c15636a28cea43e1f30a1361f7c
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-12-01 13:13:55 +0100

    first part

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,38 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  defp find_first_integer(strs) do
+    Enum.find(strs, fn str ->
+      case Integer.parse(str) do
+        {_, _rest} -> true
+        :error -> false
+      end
+    end)
+  end
+
+  defp parse_int(nil, _), do: 0
+  defp parse_int(_, nil), do: 0
+
+  defp parse_int(first, last) do
+    {int, _} = Integer.parse(first <> last)
+    int
+  end
+
+  def part1(input) do
+    input
+    |> String.split(~r{\n})
+    |> Enum.reduce(0, fn strs, acc ->
+      first =
+        String.split(strs, "")
+        |> find_first_integer()
+
+      last =
+        String.split(strs, "")
+        |> Enum.reverse()
+        |> find_first_integer()
+
+      int = parse_int(first, last)
+
+      acc + int
+    end)
   end
 
   def part2(_args) do
