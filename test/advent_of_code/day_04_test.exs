7139d85e763db46f59a61f81589d7dd575b76355
Pierre Lovergne <pierre.lovergne@gmail.com>
2023-12-05 09:57:14 +0100

    skiping activating test

--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -15,6 +15,7 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     assert result === 13
   end
 
+  @tag :skip
   test "part2" do
     input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
